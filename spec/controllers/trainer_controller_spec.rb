892494b49ebddc329a10ade7459b78b128732814
Alexander Morozov <alexander.morozov@postindustria.com>
2017-01-26 12:45:26 +0200

    gfix

--- spec/controllers/trainer_controller_spec.rb
+++ spec/controllers/trainer_controller_spec.rb
@@ -1,6 +1,14 @@
 # coding: utf-8
 require 'rails_helper'
 
+def check_message(translation, cdate, match)
+  put :review_card, params: { card_id: @card.id,
+                              user_translation: translation }, xhr: true
+  expect(@card.reload.review_date.to_date).to eq(cdate)
+  expect((flash[:notice] || flash[:alert])).to match(match)
+  expect(response).to redirect_to(trainer_path)
+end
+
 RSpec.describe Dashboard::TrainerController do
   before do
     @user = create(:user_with_one_block_and_one_card)
@@ -11,31 +19,23 @@ RSpec.describe Dashboard::TrainerController do
   describe 'PUT #review_card' do
     describe 'Corect message is shown' do
       it 'have correct answer' do
-        put :review_card, params: { card_id: @card.id, user_translation: 'house' }, xhr: true
-        expect(@card.reload.review_date.to_date).to eq(Date.today + 6)
-        expect(flash[:notice]).to match(/Вы ввели верный перевод. Продолжайте.*/)
-        expect(response).to redirect_to(trainer_path)
+        check_message('house', 6.days.from_now.to_date,
+                      /Вы ввели верный перевод. Продолжайте.*/)
       end
 
       it 'have empty answer' do
-        put :review_card, params: { card_id: @card.id, user_translation: '' }, xhr: true
-        expect(@card.reload.review_date.to_date).to eq(Date.today)
-        expect(flash[:alert]).to match(/Вы ввели не верный перевод. Повторите попытку.*/)
-        expect(response).to redirect_to(trainer_path)
+        check_message('', Date.today,
+                      /Вы ввели не верный перевод. Повторите попытку.*/)
       end
 
       it 'have incorrect answer' do
-        put :review_card, params: { card_id: @card.id, user_translation: 'translation' }, xhr: true
-        expect(@card.reload.review_date.to_date).to eq(Date.today)
-        expect(flash[:alert]).to match(/Вы ввели не верный перевод. Повторите попытку.*/)
-        expect(response).to redirect_to(trainer_path)
+        check_message('translation', Date.today,
+                      /Вы ввели не верный перевод. Повторите попытку.*/)
       end
 
       it 'have misspelling short' do
-        put :review_card, params: { card_id: @card.id, user_translation: 'housy' }, xhr: true
-        expect(@card.reload.review_date.to_date).to eq(Date.today)
-        expect(flash[:alert]).to match(/Вы ввели перевод c опечаткой.*/)
-        expect(response).to redirect_to(trainer_path)
+        check_message('housy', Date.today,
+                      /Вы ввели перевод c опечаткой.*/)
       end
     end
   end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,53 +1,55 @@
 # coding: utf-8
 require 'rails_helper'
 
+def card_errors_check(ortext, trtext, symtext, match)
+  card = Card.create(original_text: ortext, translated_text: trtext, user_id: 1,
+                     block_id: 1)
+  expect(card.errors[symtext]).to include(match)
+end
+
+def check_translation_calculation(ortext, trtext, answer, value)
+  card = Card.create(original_text: ortext, translated_text: trtext,
+                     user_id: 1, block_id: 1)
+  check_result = SuperMemo.new(card, answer).check_translation
+  expect(check_result).to eq value
+end
+
+def check_card_creation_validation(card, sym, match)
+  expect(card.errors[sym]).to include(match)
+end
+
 describe Card do
   it 'create card with empty original text' do
-    card = Card.create(original_text: '', translated_text: 'house', user_id: 1,
-                       block_id: 1)
-    expect(card.errors[:original_text]).to include('Необходимо заполнить поле.')
+    card_errors_check('', 'house', :original_text, 'Необходимо заполнить поле.')
   end
 
   it 'create card with empty translated text' do
-    card = Card.create(original_text: 'дом', translated_text: '', user_id: 1,
-                       block_id: 1)
-    expect(card.errors[:translated_text]).
-        to include('Необходимо заполнить поле.')
+    card_errors_check('дом', '', :translated_text, 'Необходимо заполнить поле.')
   end
 
   it 'create card with empty texts' do
-    card = Card.create(original_text: '', translated_text: '', user_id: 1,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-        to include('Вводимые значения должны отличаться.')
+    card_errors_check('', '', :original_text,
+                      'Вводимые значения должны отличаться.')
   end
 
   it 'equal_texts Eng' do
-    card = Card.create(original_text: 'house', translated_text: 'house',
-                       user_id: 1, block_id: 1)
-    expect(card.errors[:original_text]).
-        to include('Вводимые значения должны отличаться.')
+    card_errors_check('house', 'house', :original_text,
+                      'Вводимые значения должны отличаться.')
   end
 
   it 'equal_texts Rus' do
-    card = Card.create(original_text: 'дом', translated_text: 'дом', user_id: 1,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-        to include('Вводимые значения должны отличаться.')
+    card_errors_check('дом', 'дом', :original_text,
+                      'Вводимые значения должны отличаться.')
   end
 
   it 'full_downcase Eng' do
-    card = Card.create(original_text: 'hOuse', translated_text: 'houSe',
-                       user_id: 1, block_id: 1)
-    expect(card.errors[:original_text]).
-        to include('Вводимые значения должны отличаться.')
+    card_errors_check('hOuse', 'houSe', :original_text,
+                      'Вводимые значения должны отличаться.')
   end
 
   it 'full_downcase Rus' do
-    card = Card.create(original_text: 'Дом', translated_text: 'доМ', user_id: 1,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-        to include('Вводимые значения должны отличаться.')
+    card_errors_check('Дом', 'доМ', :original_text,
+                      'Вводимые значения должны отличаться.')
   end
 
   it 'create card original_text OK' do
@@ -71,35 +73,28 @@ describe Card do
   it 'set_review_date OK' do
     card = Card.create(original_text: 'дом', translated_text: 'house',
                        user_id: 1, block_id: 1)
-    expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-        to eq(Time.zone.now.strftime('%Y-%m-%d %H:%M'))
+    expect(card.review_date.strftime('%Y-%m-%d %H:%M'))
+      .to eq(Time.zone.now.strftime('%Y-%m-%d %H:%M'))
   end
 
   it 'create card witout user_id' do
     card = Card.create(original_text: 'дом', translated_text: 'house',
                        block_id: 1)
-    expect(card.errors[:user_id]).
-        to include('Ошибка ассоциации.')
+    check_card_creation_validation(card, :user_id, 'Ошибка ассоциации.')
   end
 
   it 'create card witout block_id' do
     card = Card.create(original_text: 'дом', translated_text: 'house',
                        user_id: 1)
-    expect(card.errors[:block_id]).
-        to include('Выберите колоду из выпадающего списка.')
+    check_card_creation_validation(card, :block_id,
+                                   'Выберите колоду из выпадающего списка.')
   end
 
   it 'check_translation Eng OK levenshtein_distance=1' do
-    card = Card.create(original_text: 'дом', translated_text: 'hou',
-                       user_id: 1, block_id: 1)
-    check_result = SuperMemo.new(card, 'house').check_translation
-    expect(check_result).to eq 2
+    check_translation_calculation('дом', 'hou', 'house', 2)
   end
 
   it 'check_translation Rus OK levenshtein_distance=1' do
-    card = Card.create(original_text: 'house', translated_text: 'до',
-                       user_id: 1, block_id: 1)
-    check_result = SuperMemo.new(card, 'дом').check_translation
-    expect(check_result).to eq 1
+    check_translation_calculation('house', 'до', 'дом', 1)
   end
 end
