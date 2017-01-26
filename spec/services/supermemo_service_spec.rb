69ef1f7e0d9f1e3136cfd38e66555b6207274113
sinpai <monaco90@gmail.com>
2017-01-26 23:58:16 +0200

    small fix

--- spec/services/supermemo_service_spec.rb
+++ spec/services/supermemo_service_spec.rb
@@ -123,13 +123,11 @@ describe SuperMemo do
       end
 
       it 'should correctly change repeat when mistyping for 2 chars' do
-        card = create_card
         expect(SuperMemo.new(card, 'homt').card_update).to be nil
         expect(card.reload.repeat).to eq(2)
       end
 
       it 'should correctly change efactor when it lower than minimum' do
-        card = create_card
         card.update_attributes(efactor: 1.1)
         SuperMemo.new(card, 'hond').card_update
         expect(card.reload.efactor.round(1)).to eq(1.3)
