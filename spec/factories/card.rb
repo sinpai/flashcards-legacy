f31d786bb5e6fb8660e0fb1c4e5567d5a297ce57
Alexander Morozov <alexander.morozov@postindustria.com>
2017-01-27 22:36:37 +0200

    last fix for task-1

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -3,9 +3,6 @@ FactoryGirl.define do
   factory :card do
     original_text 'дом'
     translated_text 'house'
-    interval 1
-    repeat 1
-    efactor 2.5
     user
     block
   end
--- spec/services/supermemo_service_spec.rb
+++ spec/services/supermemo_service_spec.rb
@@ -7,85 +7,88 @@ end
 
 describe SuperMemo do
 
-  let!(:create_card) {Card.create(original_text: 'dog', translated_text: 'hond', user_id: 1, block_id: 1)}
+  let!(:create_card) { Card.create(original_text: 'dog', translated_text: 'hond', user_id: 1, block_id: 1) }
   let!(:reloaded_card) { create_card.reload }
 
   describe 'correct cases' do
 
-    let!(:correct_answer) { SuperMemo.new(create_card, 'hond').card_update }
+    let!(:correct_answer) { SuperMemo.new(create_card, 'hond') }
+    before { correct_answer.card_update }
 
     it 'should return correct state' do
-      expect(correct_answer).to be true
+      expect(correct_answer.card_update).to be true
     end
 
     it 'should correctly change efactor when correct answer' do
-      expect(create_card.reload.efactor.round(1)).to eq(2.6)
+      expect(reloaded_card.efactor.round(1)).to eq(2.6)
     end
 
     it 'should correctly change interval when correct answer' do
-      expect(create_card.reload.interval).to eq(1)
+      expect(reloaded_card.interval).to eq(1)
     end
 
     it 'should correctly change interval when first correct attempt' do
-      expect(create_card.reload.interval).to eq(1)
+      expect(reloaded_card.interval).to eq(1)
     end
 
-    it 'should correctly change repeat when correct answer' do
-      expect(create_card.reload.repeat).to eq(2)
+    it 'should correctly chantyped_answge repeat when correct answer' do
+      expect(reloaded_card.repeat).to eq(2)
     end
 
     it 'should correctly set review_date when correct answer' do
-      expect(create_card.review_date.to_date).to eq(1.day.from_now.to_date)
+      expect(reloaded_card.review_date.to_date).to eq(1.day.from_now.to_date)
     end
   end
 
   describe 'incorrect cases' do
 
-    let!(:incorrect_answer) { SuperMemo.new(create_card, '123asd123asd').card_update }
+    let!(:incorrect_answer) { SuperMemo.new(create_card, '123asd123asd') }
+    before { incorrect_answer.card_update }
 
     it 'should return correct state when invalid answer' do
-      expect(incorrect_answer).to be nil
+      expect(incorrect_answer.card_update).to be nil
     end
 
     it 'should correctly change efactor when incorrect answer' do
-      expect(create_card.reload.efactor.round(1)).to eq(1.7)
+      expect(reloaded_card.efactor.round(1)).to eq(1.7)
     end
 
     it 'should correctly change interval when incorrect answer' do
-      expect(create_card.reload.interval).to eq(1)
+      expect(reloaded_card.interval).to eq(1)
     end
 
     it 'should correctly change repeat when incorrect answer' do
-      expect(create_card.reload.repeat).to eq(1)
+      expect(reloaded_card.repeat).to eq(1)
     end
 
     it 'should correctly set review_date when incorrect answer' do
-      expect(create_card.review_date.to_date).to eq(Date.today)
+      expect(reloaded_card.review_date.to_date).to eq(Date.today)
     end
   end
 
   describe 'mistyped cases' do
 
-    let!(:mistyped_answer) { SuperMemo.new(create_card, 'hont').card_update }
+    let!(:mistyped_answer) { SuperMemo.new(create_card, 'hont') }
+    before { mistyped_answer.card_update }
 
     it 'should return correct state when mistype' do
-      expect(mistyped_answer).to be false
+      expect(mistyped_answer.card_update).to be false
     end
 
     it 'should correctly change efactor when there was mistyping' do
-      expect(create_card.reload.efactor.round(1)).to eq(2.5)
+      expect(reloaded_card.efactor.round(1)).to eq(2.5)
     end
 
     it 'should correctly change interval when mistyping' do
-      expect(create_card.reload.interval).to eq(1)
+      expect(reloaded_card.interval).to eq(1)
     end
 
     it 'should correctly change repeat when mistyping' do
-      expect(create_card.reload.repeat).to eq(2)
+      expect(reloaded_card.reload.repeat).to eq(2)
     end
 
     it 'should correctly set review_date when incorrect answer' do
-      expect(create_card.review_date.to_date).to eq(Date.today)
+      expect(reloaded_card.review_date.to_date).to eq(Date.today)
     end
   end
 
@@ -110,34 +113,34 @@ describe SuperMemo do
 
       describe 'with correct answer' do
 
-        let(:studied) { SuperMemo.new(create_card, 'hond') }
+        let!(:studied) { SuperMemo.new(create_card, 'hond') }
 
         it 'should correctly change interval when 2 correct answers' do
           expect(2.times { studied.card_update }).to be 2
-          expect(create_card.interval).to eq(6)
+          expect(reloaded_card.interval).to eq(6)
         end
 
         it 'should correctly change interval when third correct attempt' do
           expect(3.times { studied.card_update }).to be 3
-          expect(create_card.reload.interval).to eq(17)
+          expect(reloaded_card.interval).to eq(17)
         end
 
         it 'should correctly set review_date when 4 correct answers made' do
           4.times { studied.card_update }
-          expect(create_card.review_date.to_date).to eq(49.day.from_now.to_date)
+          expect(reloaded_card.review_date.to_date).to eq(49.day.from_now.to_date)
         end
 
         it 'should correctly change efactor when it lower than minimum' do
           create_card.update_attributes(efactor: 1.1)
           studied.card_update
-          expect(create_card.reload.efactor.round(1)).to eq(1.3)
+          expect(reloaded_card.efactor.round(1)).to eq(1.3)
         end
 
       end
 
       it 'should correctly change repeat when mistyping for 2 chars' do
         expect(SuperMemo.new(create_card, 'homt').card_update).to be nil
-        expect(create_card.reload.repeat).to eq(2)
+        expect(reloaded_card.repeat).to eq(2)
       end
 
     end
